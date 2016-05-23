9f0fcd17743d5b36340a3b843df56a23e5b7d064
shailesh108 <sp98484@gmail.com>
2016-05-23 18:53:48 +0530

    gmail and yahoo contact import

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,19 @@
+class MainController < ApplicationController
+	def index
+	end
+	def contact_yahoo
+		@contacts = request.env['omnicontacts.contacts']
+		respond_to do |format|
+			format.html 
+		end
+	end
+
+def contact
+	@contacts = request.env['omnicontacts.contacts']
+	respond_to do |format|
+		format.html 
+
+	end
+end
+
+end
